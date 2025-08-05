1df34da0f9215810c8de93f8628e67d61b5e3197
Rashmi Rashmi <rashmira@sheridancollege.ca>
2025-08-05 13:54:27 -0400

    added ability to register and update code logic

---
+++ src/ca/sheridancollege/project/Main.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author rashm
+ */
+public class Main {
+    public static void main(String[] args) {
+        GroupOfCards deck = new GroupOfCards(52);
+        deck.generateDeck();
+        deck.shuffle();
+
+        WarPlayer p1 = new WarPlayer("Ash");
+        WarPlayer p2 = new WarPlayer("Misty");
+
+        for (int i = 0; i < 52; i++) {
+            if (i % 2 == 0) {
+                p1.addToHand(List.of(deck.getCards().get(i)));
+            } else {
+                p2.addToHand(List.of(deck.getCards().get(i)));
+            }
+        }
+
+   
+
+        WarGame game = new WarGame("War Card Game");
+        game.registerPlayer(p1);
+        game.registerPlayer(p2);
+
+        game.play();
+        game.declareWinner();    }
+}
+    
+
