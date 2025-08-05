215d3da89dd3f1266e245f9a6f8e84ddc4674e63
Rashmi Rashmi <rashmira@sheridancollege.ca>
2025-08-05 14:11:55 -0400

    added ability to register and update code logic

---
+++ src/ca/sheridancollege/project/PlayingCard.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author rashm
+ */
+public class PlayingCard extends Card {
+    public PlayingCard(String suit, int rank) {
+        super(suit, rank);
+    }
+}
+
+    
