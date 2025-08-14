3260fa13c19d04b9a654183e3c329f3629e25b2d
Rashmi Rashmi <rashmira@sheridancollege.ca>
2025-08-14 12:30:09 -0400

    instead of whole game, limited rounds to upto 10

--- src/ca/sheridancollege/project/WarGame.java
+++ src/ca/sheridancollege/project/WarGame.java
@@ -4,14 +4,16 @@
  */
 package ca.sheridancollege.project;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 /**
  *
  * @author rashm
  */
-public class WarGame extends Game {public WarGame(String name) {
+public class WarGame extends Game {
+     private int roundNumber = 10;
+    public WarGame(String name) {
         super(name);
     }
 
@@ -19,23 +21,31 @@ public class WarGame extends Game {public WarGame(String name) {
     public void play() {
         Player p1 = getPlayers().get(0);
         Player p2 = getPlayers().get(1);
-        while (p1.hasCards() && p2.hasCards()) {
-            
-            System.out.println("\nCurrent Cards for players: " + p1.getName() + " = " + p1.getCardCount() + ", " + p2.getName() + " = " + p2.getCardCount());
-            Card[] cards = Round.playRound(p1, p2);
-            int result = Round.compareCards(cards[0], cards[1]);
+        for (int round = 1; round <= roundNumber; round++) {
+            System.out.println("\n Round " + round);
+
+            Card c1 = ((WarPlayer) p1).playCard();
+            Card c2 = ((WarPlayer) p2).playCard();
+
+            if (c1 == null || c2 == null) {
+                System.out.println("The end");
+                break;
+            }
+
+            System.out.println(p1.getName() + " plays " + c1);
+            System.out.println(p2.getName() + " plays " + c2);
+
+            int result = Round.compareCards(c1, c2);
+
             if (result > 0) {
-                List<Card> won = new ArrayList<>();
-                won.add(cards[0]);
-                won.add(cards[1]);
-                p1.collectCards(won);
+                System.out.println(p1.getName() + " wins this round");
+                ((WarPlayer) p1).collectCards(List.of(c1, c2));
             } else if (result < 0) {
-                List<Card> won = new ArrayList<>();
-                won.add(cards[0]);
-                won.add(cards[1]);
-                p2.collectCards(won);
+                System.out.println(p2.getName() + " wins this round");
+                ((WarPlayer) p2).collectCards(List.of(c1, c2));
             } else {
-                WarHandler.resolveWar(cards[0], cards[1], p1, p2);
+                System.out.println("War!");
+                WarHandler.resolveWar(c1, c2, (WarPlayer) p1, (WarPlayer) p2);
             }
         }
     }@Override
@@ -43,11 +53,11 @@ public class WarGame extends Game {public WarGame(String name) {
         Player p1 = getPlayers().get(0);
         Player p2 = getPlayers().get(1);
         if (p1.hasCards()) {
-            System.out.println(p1.getName() + " wins the game!");
+            System.out.println(p1.getName() + " wins the game.");
         } else if (p2.hasCards()) {
-            System.out.println(p2.getName() + " wins the game!");
+            System.out.println(p2.getName() + " wins the game.");
         } else {
-            System.out.println("It's a tie!");
+            System.out.println("It's a tie");
         }
     }
 
